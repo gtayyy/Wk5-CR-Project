e4a30fbf8724612c4575c8064e0e34c5fa1a17c2
G_T_Ayyy <147361574+gtayyy@users.noreply.github.com>
2023-11-27 13:51:31 -0800

    Tests 1,2,3 pass

---
+++ src/js/planetyears.js
@@ -0,0 +1,28 @@
+//business logic
+
+export class Mercury {
+  constructor(earthAge) {
+    this.mercuryYears = earthAge * 0.24;
+  }
+  getMercuryAge() {
+    return this.mercuryYears;
+  }
+}
+
+export class Venus {
+	constructor(earthAge) {
+		this.venusYears = earthAge * 0.62;
+	}
+	getVenusAge() {
+		return this.venusYears;
+	}
+}
+
+export class Mars {
+	constructor(earthAge) {
+		this.marsYears = earthAge * 6.91;
+	}
+	getMarsAge() {
+		return this.marsYears;
+	}
+}
