7beca9d635902264de21422dfc5f84fd62d71d48
G_T_Ayyy <147361574+gtayyy@users.noreply.github.com>
2023-11-27 12:00:56 -0800

    Good red test

--- __tests__/planetyears.test.js
+++ __tests__/planetyears.test.js
@@ -3,8 +3,11 @@
 // Mars: 1.88 earth = 1 yr
 // Jupiter: 11.86 earth  = 1 yr
 
+import Mercury from './../src/js/planetyears.js'
+
+
 describe('Mercury', () => {
-	test('should return the users input age in equivalent Mercury years'() => {
+	test('should return the users input age in equivalent Mercury years',() => {
 		const mercury = new Mercury(35);
 		expect(mercury.years).toEqual(8.4);
 	});
