36824fd54ec137467620be88b4ca801eb01eb0e0
G_T_Ayyy <147361574+gtayyy@users.noreply.github.com>
2023-11-27 12:52:13 -0800

    Green pass.

--- __tests__/planetyears.test.js
+++ __tests__/planetyears.test.js
@@ -3,13 +3,22 @@
 // Mars: 1.88 earth = 1 yr
 // Jupiter: 11.86 earth  = 1 yr
 
-import Mercury from './../src/js/planetyears.js'
+import { Mercury } from './../src/js/planetyears.js'
 
 
 describe('Mercury', () => {
-	test('should return the users input age in equivalent Mercury years',() => {
-		const mercury = new Mercury(35);
-		expect(mercury.years).toEqual(8.4);
+	test('should return the users input age in equivalent Mercury years', () => {
+		const earthAge = 35;
+		const mercury = new Mercury(earthAge);
+		expect(mercury.getMercuryAge()).toBeCloseTo(8.4);
 	});
 })
+
+
+// describe('Mercury class', () => {
+//  test('should calculate Mercury age correctly', () => {
+//    const earthAge = 25;
+//    const mercury = new Mercury(earthAge);
+//    expect(mercury.getMercuryAge()).toBeCloseTo(earthAge * 0.24, 10); // Using toBeCloseTo due to potential floating-point precision issues
+//  });
 
