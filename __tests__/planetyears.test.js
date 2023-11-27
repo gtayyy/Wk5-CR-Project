a853043320ae52b97e638503da17df7c9b7d9c7c
G_T_Ayyy <147361574+gtayyy@users.noreply.github.com>
2023-11-27 11:36:35 -0800

    Mercury test written

---
+++ __tests__/planetyears.test.js
@@ -0,0 +1,12 @@
+// Mercury: .24 earth = 1 yr
+// Venus: .62 earth = 1 yr
+// Mars: 1.88 earth = 1 yr
+// Jupiter: 11.86 earth  = 1 yr
+
+describe('Mercury', () => {
+	test('should return the users input age in equivalent Mercury years'() => {
+		const mercury = new Mercury(35);
+		expect(mercury.years).toEqual(8.4);
+	});
+})
+
