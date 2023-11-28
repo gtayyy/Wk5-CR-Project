41bb53024540afcafc0b6b299d7c3627089aeaa4
G_T_Ayyy <147361574+gtayyy@users.noreply.github.com>
2023-11-28 14:04:37 -0800

    Yrs until Venus green pass

--- __tests__/planetyears.test.js
+++ __tests__/planetyears.test.js
@@ -122,12 +122,12 @@ describe('YearsUntilMerc', () => {
 
 describe('YearsUntilVenus', () => {
   test('should return the number of Venus years from the users current age until a future given age', () => {
-    // const currentAge = 35;
-    // const futureAge = 50;
-    // const yrsDelta = futureAge - currentAge;
-    // const yrsUntilVenus = new YearsUntilVenus(yrsDelta);
-    // const venusConversion = 0.62;
-    // const expectedVenusYears = yrsUntilVenus.getDeltaAge() / venusConversion;
+    const currentAge = 35;
+    const futureAge = 50;
+    const yrsDelta = futureAge - currentAge;
+    const yrsUntilVenus = new YearsUntilVenus(yrsDelta);
+    const venusConversion = 0.62;
+    const expectedVenusYears = yrsUntilVenus.getDeltaAge() / venusConversion;
     expect(yrsUntilVenus.getDeltaUntilVenus()).toBeCloseTo(expectedVenusYears);
   });
 });
