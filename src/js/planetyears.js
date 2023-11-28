40bd2ccabe4240f45d5f27f8694728edf69c32bf
G_T_Ayyy <147361574+gtayyy@users.noreply.github.com>
2023-11-27 16:26:56 -0800

    add years since Venus

--- src/js/planetyears.js
+++ src/js/planetyears.js
@@ -6,63 +6,51 @@
 
 export class Mercury {
   constructor(earthAge) {
-    this.mercuryYears = earthAge * 0.24;
+    this.mercuryYears = earthAge / 0.24;
   }
   getMercuryAge() {
     return this.mercuryYears;
   }
-}
+};
 
 export class Venus {
 	constructor(earthAge) {
-		this.venusYears = earthAge * 0.62;
+		this.venusYears = earthAge / 0.62;
 	}
 	getVenusAge() {
 		return this.venusYears;
 	}
-}
+};
 
 export class Mars {
 	constructor(earthAge) {
-		this.marsYears = earthAge * 1.88;
+		this.marsYears = earthAge / 1.88;
 	}
 	getMarsAge() {
 		return this.marsYears;
 	}
-}
+};
 
 export class Jupiter {
 	constructor(earthAge) {
-		this.jupiterYears = earthAge * 11.86;
+		this.jupiterYears = earthAge / 11.86;
 	}
 	getJupiterAge() {
 		return this.jupiterYears;
 	}
-}
-
-// describe('Jupiter', () => {
-// 	test('should return the users input age in equivalent Jupiter years', () => {
-// 		const earthAge = 35;
-// 		const jupiter = new Jupiter(earthAge);
-// 		expect(jupiter.getJupiterAge()).toBeCloseTo(415.1);
-// 	});
-// })
+};
 
 export class YearsSince {
-	constructor(yrsDelta) {
-		this.yrsSince = yrsDelta;  
+  constructor(yrsDelta) {
+    this.yrsSince = yrsDelta;
+  }
+  getDeltaAge() {
+    return this.yrsSince;
+  }
+  getDeltaAgeInMercury() {
+    return this.yrsSince / 0.24;
 	}
-	getDeltaAge() {
-		return this.yrsSince;
+	getDeltaAgeInVenus() {
+		return this.yrsSince / 0.62;
 	}
-}
-
-// describe('YearsSince', () => {
-// 	test('should return the number of years that have passed since a given age', () => {
-// 	const previousAge = 21;
-// 	const currentAge = 35;
-// 	const yrsDelta = currentAge - previousAge;
-	
-// 		expect(yrsSince.getYearsSinceNum()).toBeCloseTo();
-// 	});
-// })
+};
